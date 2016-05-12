06620c3961b4e268bc101c22c8b7fed3b140f875
wallaceyuan <1272864289@qq.com>
2016-05-12 15:20:47 +0800

    init

---
+++ es6/1.Let-Const.js
@@ -0,0 +1,19 @@
+/**
+ * Created by yuan on 2016/5/12.
+ */
+// demo 1
+/*
+function f1() {
+    let n = 5;
+    if (true) {
+        let n = 10;
+    }
+    console.log(n); // 5
+}
+
+// demo 2
+const PI = 3.1415;
+console.log(PI); // 3.1415
+
+PI = 3;
+console.log(PI); // TypeError: "PI" is read-only*/
