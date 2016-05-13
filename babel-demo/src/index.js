9647aa72546d8a87e61db22b696ea1e188d14a77
wallaceyuan <1272864289@qq.com>
2016-05-13 09:54:10 +0800

    babel

---
+++ babel-demo/src/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by yuan on 2016/5/12.
+ */
+
+import "babel-polyfill";
+
+console.log(Array.from('aaaa'));
+
+console.log('=======');
+
+const sum = (a,b)=>a+b;
+
+let x = sum(1,2);
+let y = sum(1,2);
+
+
+console.log(x);
---
+++ babel-demo/src/jsx.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yuan on 2016/5/12.
+ */
+
+import "babel-polyfill";
+
+import React,{Component} from 'react'
+
+class DemoComponent extends Component{
+    render(){
+        return <h1>Hello World!</h1>
+    }
+}
